4f81ec24f4c6c73ea7d969878ed727ae6a908f24
Emile de Kadt <62763004+edekadt@users.noreply.github.com>
2025-03-13 16:40:30 +0100

    Script de scroll
    Número 1 en razones por las que Unity ha fallado a la humanidad y debe ser destruido:
    Para hacer un maldito sprite con scroll infinito he tenido que recurrir a reflexión.

--- La_Rata_Tostada_se_va_por_las_Ramas/Assets/Scripts/ImgScroll.cs
+++ La_Rata_Tostada_se_va_por_las_Ramas/Assets/Scripts/ImgScroll.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,21 +15,71 @@ public class ImgScroll : MonoBehaviour
 
     bool scrolling;
 
-    RawImage img;
+    SpriteRenderer img;
+
+    Vector3 startingPos;
 
     // En el caso de que se hagan varias llamadas a pausar el scroll, esto asegura que no se retoma hasta que caduquen todas las pausas
     private int pauseCounter;
 
+    [SerializeField, Tooltip("Gameobjects auxiliares que se usan para crear las extensiones del sprite. Deben ser hijos vacíos de este objeto.")]
+    GameObject child1;
+    [SerializeField, Tooltip("Gameobjects auxiliares que se usan para crear las extensiones del sprite. Deben ser hijos vacíos de este objeto.")]
+    GameObject child2;
+
     private void Start()
     {
-        img = GetComponent<RawImage>();
+        img = GetComponent<SpriteRenderer>();
         scrolling = true;
+        startingPos = transform.position;
+
+        if (scrollDirection.x > 0)
+        {
+            child1.AddComponent(img);
+            child1.transform.position = new Vector3(transform.position.x - img.bounds.size.x, transform.position.y, transform.position.z);
+        }
+        else if (scrollDirection.x < 0)
+        {
+            child1.AddComponent(img);
+            child1.transform.position = new Vector3(transform.position.x + img.bounds.size.x, transform.position.y, transform.position.z);
+        }
+
+        if (scrollDirection.y > 0)
+        {
+            child1.AddComponent(img);
+            child1.transform.position = new Vector3(transform.position.x, transform.position.y - img.bounds.size.y, transform.position.z);
+        }
+        else if (scrollDirection.y < 0)
+        {
+            child1.AddComponent(img);
+            child1.transform.position = new Vector3(transform.position.x, transform.position.y + img.bounds.size.y, transform.position.z);
+        }
     }
 
     void Update()
     {
         if (scrolling)
-            img.uvRect = new Rect(img.uvRect.position + scrollDirection.normalized * scrollSpeed * Time.deltaTime, img.uvRect.size);
+        {
+            transform.position = new Vector3 (transform.position.x + scrollDirection.normalized.x * scrollSpeed * Time.deltaTime, 
+                transform.position.y + scrollDirection.normalized.y * scrollSpeed * Time.deltaTime, 0f);
+
+            if (Math.Abs(transform.position.x - startingPos.x) > img.bounds.size.x)
+            {
+                if (transform.position.x > startingPos.x)
+                    transform.position = new Vector3(transform.position.x - img.bounds.size.x, transform.position.y, transform.position.z);
+                else
+                    transform.position = new Vector3(transform.position.x + img.bounds.size.x, transform.position.y, transform.position.z);
+            }
+
+            if (Math.Abs(transform.position.y - startingPos.y) > img.bounds.size.y)
+            {
+                if (transform.position.y > startingPos.y)
+                    transform.position = new Vector3(transform.position.x, transform.position.y - img.bounds.size.y, transform.position.z);
+                else
+                    transform.position = new Vector3(transform.position.x, transform.position.y + img.bounds.size.y, transform.position.z);
+            }
+        }
+            //img.uvRect = new Rect(img.uvRect.position + scrollDirection.normalized * scrollSpeed * Time.deltaTime, img.uvRect.size);
     }
 
     void pauseScroll(float seconds)
