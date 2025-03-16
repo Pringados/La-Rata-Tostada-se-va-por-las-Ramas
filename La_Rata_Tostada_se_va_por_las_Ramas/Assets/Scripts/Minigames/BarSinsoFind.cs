ac754ffeb13fb96a0cda63ed35319b6ad584f9fa
Erisolo <marsolor@ucm.es>
2025-03-16 04:25:21 +0100

    LO CONSEGUÍ. ya no hay objetos detrás de la barra

--- La_Rata_Tostada_se_va_por_las_Ramas/Assets/Scripts/Minigames/BarSinsoFind.cs
+++ La_Rata_Tostada_se_va_por_las_Ramas/Assets/Scripts/Minigames/BarSinsoFind.cs
@@ -28,6 +28,10 @@ public class BarSinsoFind : IMinigame
     [SerializeField]
     GameObject whatToFind;
 
+    [SerializeField]
+    GameObject forbiddenSquare;
+    Vector2 forbIni, forbFin;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -35,6 +39,20 @@ public class BarSinsoFind : IMinigame
         canvaY = canvas.GetComponent<RectTransform>().rect.height / 2;
         //primero decidimos q objeto vamos a encontrar:
         objToFind = Random.Range(0, instanciableObjects.Count);
+
+        //calculamos el cuadrado donde no se puede spawnear
+        RectTransform forbTrans = forbiddenSquare.GetComponent<RectTransform>();
+        Vector3[] corners = new Vector3[4];
+        forbTrans.GetWorldCorners(corners);
+        //forbIni = corners[0];
+
+        Vector2 forbLeftBottom, forbRightTop;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.GetComponent<RectTransform>(), corners[0], canvas.transform.GetComponentInParent<Canvas>().worldCamera, out forbLeftBottom);
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.GetComponent<RectTransform>(), corners[2], canvas.transform.GetComponentInParent<Canvas>().worldCamera, out forbRightTop);
+
+        forbIni.x = forbLeftBottom.x;
+        forbIni.y = forbRightTop.y;
+        Debug.Log(forbIni);
 
         //instanciamos todos los objetos en posiciones Random
         for (int i = 0; i < instanciableObjects.Count; i++)
@@ -44,12 +62,18 @@ public class BarSinsoFind : IMinigame
                 int rep = Random.Range(minObjects, maxObjects);
                 for (int j = 0; j < rep; j++)
                 {
+                    Vector2 pos = new Vector2(Random.Range(-canvaX + paddinginx, canvaX - paddinginx), Random.Range(-canvaY+10, -offsetinY));
+                    while (pos.x >= forbIni.x && pos.y <= forbIni.y)    //si está en el forbREct
+                    {
+                        pos = new Vector2(Random.Range(-canvaX + paddinginx, canvaX - paddinginx), Random.Range(-canvaY+10, -offsetinY));
+                    }
+                    Debug.Log(pos);
                     GameObject newButton = new GameObject();
                     RectTransform trans = newButton.AddComponent<RectTransform>();
 
                     newButton.AddComponent<CanvasRenderer>();
                     trans.SetParent(canvas.transform);
-                    trans.anchoredPosition = new Vector2(Random.Range(-canvaX+paddinginx, canvaX-paddinginx), Random.Range(-canvaY, -offsetinY));
+                    trans.anchoredPosition = pos;
 
                     Image img = newButton.AddComponent<Image>();
                     img.sprite = instanciableObjects[i];
