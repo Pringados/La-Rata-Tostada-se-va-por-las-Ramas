550c513561cb0c9b3d0cbacbee275eb4e7c09efe
Erisolo <marsolor@ucm.es>
2025-03-16 18:31:15 +0100

    Los dialogos funcionan alegria alegria

--- La_Rata_Tostada_se_va_por_las_Ramas/Assets/Scripts/NPC controller.cs
+++ La_Rata_Tostada_se_va_por_las_Ramas/Assets/Scripts/NPC controller.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Linq;
+using System;
 
 public class NPCcontroller : MonoBehaviour
 {
@@ -13,13 +15,19 @@ public class NPCcontroller : MonoBehaviour
     [SerializeField]
     GameObject button;
     NPCData data;
+    int actDiag, actLine = 0;
     // Start is called before the first frame update
     void Start()
     {
         //ponemos un maravilloso npc en su sitio
         data = MapManager.instance.getNPCData();
         NPCSprite.GetComponent<Image>().sprite = data.sprite;
-        text.GetComponent<TMP_Text>().text = "multiplicate por 0";//data.dialogue[0].array[0].text;
+
+        //seleccionamos un dialogo aleatoriamente
+        actDiag = UnityEngine.Random.Range(0, data.dialogue.Count());
+
+        //la ponemos
+        text.GetComponent<TMP_Text>().text = data.dialogue[actDiag].array[actLine].text;
         Debug.Log("npcInstanciado");
 
         button.GetComponent<Button>().onClick.AddListener(onButtonPush);
@@ -36,9 +44,16 @@ public class NPCcontroller : MonoBehaviour
     private void onButtonPush()
     {
         //comprobación de si queda algo q decir
-        Debug.Log("a cambiar de escena");
-        GameManager.instance.ChangeScene(MapManager.instance.getDestino());
-        Debug.Log("supuestamente hemos cambiado la escena jefe");
+        actLine++;
+        if(actLine >= data.dialogue[actDiag].array.Count())
+        {
+            Debug.Log("a cambiar de escena");
+            GameManager.instance.ChangeScene(MapManager.instance.getDestino());
+        }
+        else
+        {
+            text.GetComponent<TMP_Text>().text = data.dialogue[actDiag].array[actLine].text;
+        }
 
     }
 }
